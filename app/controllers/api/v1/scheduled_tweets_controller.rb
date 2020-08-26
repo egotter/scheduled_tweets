a3c9684184763c624ba87a53caf2b2455ed28f06
ts-3156 <ts_3156@yahoo.co.jp>
2020-08-27 03:13:37 +0900

    Implement ScheduledTweetTime for validation

--- app/controllers/api/v1/scheduled_tweets_controller.rb
+++ app/controllers/api/v1/scheduled_tweets_controller.rb
@@ -13,16 +13,22 @@ class Api::V1::ScheduledTweetsController < ApiController
     if params['input-image']
       uploaded_file = UploadedFile.new(size: params['input-image'].size, content_type: params['input-image'].content_type)
       unless uploaded_file.valid?
-        render json: {error: uploaded_file.errors.full_messages, error_attributes: uploaded_file.errors.keys, record: uploaded_file}, status: :unprocessable_entity
+        render json: {error: uploaded_file.errors.full_messages}, status: :unprocessable_entity
         return
       end
+    end
+
+    datetime = ScheduledTweetTime.new(date: params['scheduled-date'], time: params['scheduled-time'])
+    unless datetime.valid?
+      render json: {error: datetime.errors.full_messages}, status: :unprocessable_entity
+      return
     end
 
     tweet = ScheduledTweet.new(
         user_id: current_user.id,
         text: params[:tweet_text],
-        specified_date: params['scheduled-date'],
-        specified_time: params['scheduled-time']
+        time: datetime.datetime,
+        repeat_type: params['repeat_type'],
     )
 
     if tweet.save
@@ -32,9 +38,9 @@ class Api::V1::ScheduledTweetsController < ApiController
 
       CreateTweetWorker.perform_at(tweet.time, tweet.id, user_id: current_user.id, text: tweet.text)
       message = I18n.t('scheduled_tweets.create.message', time: tweet.time.in_time_zone('Tokyo').strftime("%Y/%m/%d %H:%M"))
-      render json: {message: message, record: tweet}
+      render json: {message: message}
     else
-      response = {error: tweet.errors.full_messages, error_attributes: tweet.errors.keys, record: tweet}
+      response = {error: tweet.errors.full_messages}
       logger.warn "tweet.save failed #{response.inspect}"
       render json: response, status: :unprocessable_entity
     end
--- app/models/scheduled_tweet.rb
+++ app/models/scheduled_tweet.rb
@@ -2,23 +2,6 @@ class ScheduledTweet < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-  attr_accessor :specified_date, :specified_time
-
-  DATE_REGEXP = /\A\d{4}[\/-]\d{2}[\/-]\d{2}\z/
-  TIME_REGEXP = /\A\d{2}:\d{2}\z/
-
-  before_validation do
-    if valid_specified_date? && valid_specified_time?
-      self.time = specified_datetime
-    end
-  end
-
-  after_validation do
-    if errors.any? && errors.has_key?(:time) && (errors.has_key?(:specified_date) || errors.has_key?(:specified_time))
-      errors.delete(:time)
-    end
-  end
-
   validates :text, presence: true
   validates :time, presence: true
 
@@ -30,24 +13,6 @@ class ScheduledTweet < ApplicationRecord
     if text.to_s.include?('*')
       errors.add(:text, :text_invalid)
     end
-
-    unless valid_specified_date?
-      errors.add(:specified_date, :required)
-    end
-
-    unless valid_specified_time?
-      errors.add(:specified_time, :required)
-    end
-
-    if valid_specified_date? && valid_specified_time?
-      if specified_datetime_in_the_past?
-        errors.add(:specified_date, :cannot_be_in_the_past)
-        errors.add(:specified_time, :cannot_be_in_the_past)
-      elsif specified_datetime_in_the_distant_future?
-        errors.add(:specified_date, :cannot_be_in_the_distant_future)
-        errors.add(:specified_time, :cannot_be_in_the_distant_future)
-      end
-    end
   end
 
   scope :published, -> do
@@ -58,37 +23,11 @@ class ScheduledTweet < ApplicationRecord
     where(published_at: nil)
   end
 
-  def valid_specified_date?
-    specified_date.to_s.match?(DATE_REGEXP)
-  end
-
-  def valid_specified_time?
-    specified_time.to_s.match?(TIME_REGEXP)
-  end
-
-  def specified_datetime
-    "#{specified_date} #{specified_time}".in_time_zone('Tokyo')
-  end
-
-  def specified_datetime_in_the_past?
-    specified_datetime < 1.minute.since
-  end
-
-  def specified_datetime_in_the_distant_future?
-    specified_datetime > self.class.max_date
-  end
-
   def published?
     !published_at.nil?
   end
 
   def publish_time
     published? ? published_at : time
-  end
-
-  class << self
-    def max_date
-      1.year.since.in_time_zone('Tokyo').to_date
-    end
   end
 end
