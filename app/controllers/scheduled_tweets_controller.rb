33f25b77edab30b55efaa5df92e4729f8c6cd9db
ts-3156 <ts_3156@yahoo.co.jp>
2020-12-10 02:19:33 +0900

    Assign @published_tweets and @failed_tweets

--- app/controllers/scheduled_tweets_controller.rb
+++ app/controllers/scheduled_tweets_controller.rb
@@ -2,6 +2,16 @@ class ScheduledTweetsController < ApplicationController
   def index
     @preview_user = PreviewUser.new(user: current_user)
     @preview_tweet = ScheduledTweet.new(text: '', time: Time.zone.now)
-    @scheduled_tweets = user_signed_in? ? current_user.scheduled_tweets : ScheduledTweet.none
+
+    if user_signed_in?
+      tweets = current_user.scheduled_tweets
+      @scheduled_tweets = tweets.will_be_published
+      @published_tweets = tweets.already_published.limit(10)
+      @failed_tweets = tweets.failed_to_publish.limit(10)
+    else
+      @scheduled_tweets = ScheduledTweet.none
+      @published_tweets = ScheduledTweet.none
+      @failed_tweets = ScheduledTweet.none
+    end
   end
 end
